a75b02fe79a60accf85cb79d3983bc9267858dab
charlizebananice <charlizezmu@gmail.com>
2022-11-21 22:16:54 +0800

    web环境搭建

---
+++ src/main/resources/createDatabase.sql
@@ -0,0 +1,47 @@
+DROP TABLE manager CASCADE;
+
+CREATE TABLE tbl_manager(
+	managerID serial NOT NULL UNIQUE,
+	managerName VARCHAR(20) NOT NULL UNIQUE,
+	password VARCHAR(20) NOT NULL UNIQUE,
+	PRIMARY KEY(managerID)
+);
+DROP TABLE tbl_order CASCADE;
+
+CREATE TABLE tbl_order(
+	orderID serial NOT NULL UNIQUE,
+	state INTEGER NOT NULL,
+	fee int NOT NULL,
+	shippingAddress VARCHAR(50) NOT NULL,
+	receiveAddress VARCHAR(50) NOT NULL,
+	managerID serial,
+	deliveryManID serial,
+	PRIMARY KEY(orderID),
+	FOREIGN KEY (managerID) REFERENCES tbl_manager(managerID),
+	FOREIGN KEY (deliveryManID) REFERENCES tbl_deliveryMan(deliveryManID)
+);
+DROP TABLE tbl_deliveryMan CASCADE;
+CREATE TABLE tbl_deliveryMan(
+	deliveryManID serial NOT NULL UNIQUE,
+	deliveryManName VARCHAR(20) NOT NULL UNIQUE,
+	password VARCHAR(20) NOT NULL UNIQUE,
+	phoneNo CHAR(11) UNIQUE,
+	email VARCHAR(30) UNIQUE,
+	PRIMARY KEY(deliveryManID)
+);
+
+INSERT INTO tbl_manager(managerName,password) VALUES('root','root');
+INSERT INTO tbl_manager(managerName,password) VALUES('zmy','zmy');
+INSERT INTO tbl_manager(managerName,password) VALUES('test','test');
+select * from tbl_manager;
+delete from tbl_manager where managerID = 1;
+
+INSERT INTO tbl_deliveryMan(deliveryManName,password) VALUES('deliveryMan1','deliveryMan1');
+INSERT INTO tbl_deliveryMan(deliveryManName,password) VALUES('deliveryMan2','deliveryMan2');
+select * from tbl_deliveryMan;
+
+INSERT INTO tbl_order(state,fee,shippingAddress,receiveAddress,managerID,deliveryManID)
+VALUES('0',100, '西工大东大村', '西工大海天苑2号楼', 2,1);
+INSERT INTO tbl_order(state,fee,shippingAddress,receiveAddress,managerID)
+VALUES('0',100, '西工大东大村', '西工大海天苑2号楼', 2);
+select * from tbl_order;
