e69218f69bfb7cce11b44da2e903e5f423942033
clearlv7 <986020260@qq.com>
2022-11-23 14:43:44 +0800

    测试是否能够成功commit

--- src/main/java/nwpu/web/dao/ManagerDao.java
+++ src/main/java/nwpu/web/dao/ManagerDao.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface ManagerDao {
     @Insert("insert into tbl_manager (managerName, password) values (#{managerName},#{password})")
     public int save(@Param("manager") Manager manager);
