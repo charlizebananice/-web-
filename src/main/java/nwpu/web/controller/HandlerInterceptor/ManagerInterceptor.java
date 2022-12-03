490038499d5c72cd6aa42b59d34595406ca7342b
charlizebananice <charlizezmu@gmail.com>
2022-12-03 13:54:58 +0800

    管理员界面美化

---
+++ src/main/java/nwpu/web/controller/HandlerInterceptor/ManagerInterceptor.java
@@ -0,0 +1,38 @@
+package nwpu.web.controller.HandlerInterceptor;
+
+import nwpu.web.domain.entity.Manager;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Component
+//定义拦截器类，实现HandlerInterceptor接口
+public class ManagerInterceptor implements HandlerInterceptor {
+    //原始方法调用前执行的内容
+    //用户已登录返回true，否则返回false
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse
+            response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        Manager manager = (Manager)session.getAttribute("Manager");
+        if (manager!=null){
+            return true;
+        }else {
+            response.sendRedirect(request.getContextPath()+"/manager/login");
+            return false;
+        }
+    }
+
+    //原始方法调用后执行的内容
+    public void postHandle(HttpServletRequest request, HttpServletResponse
+            response, Object handler, ModelAndView modelAndView) throws Exception {
+    }
+
+    //原始方法调用完成后执行的内容
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    }
+}
